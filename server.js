9b3a4dc5dad68c6d52227f7aabb496969d904ab1
minkyeong <mgk04222@naver.com>
2023-01-09 19:14:30 +0900

    -

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
 
 var db;
 MongoClient.connect(
@@ -30,15 +31,63 @@ app.get("/beauty", (req, res) => {
   res.send("뷰티 관련 페이지 입니다.");
 });
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
 });
 
 app.get("/write", (req, res) => {
-  res.sendFile(__dirname + "/write.html");
+  res.render("write.ejs");
+});
+// 어떤사람이 post 요청하면(폼전송하면)
+app.post("/add", (req, res) => {
+  res.redirect("/list");
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (error, result) {
+      var 총게시물갯수 = result.totalPost;
+
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: req.body.title, 날짜: req.body.date },
+        function (error, result) {
+          console.log("저장완료");
+        }
+      );
+      //+ counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함
+      db.collection("counter").updateOne(
+        { name: "게시물갯수" },
+        { $inc: { totalPost: 1 } },
+        function (error, result) {
+          if (error) {
+            return console.log(error);
+          }
+        }
+      );
+    }
+  );
+});
+// /list로 GET요청으로 접속하면 HTML을 보여줌
+app.get("/list", (req, res) => {
+  //디비에 저장된 post라는 collection안의 모든데이터를 꺼내주세요
+  db.collection("post")
+    .find()
+    .toArray(function (error, result) {
+      res.render("list.ejs", { posts: result });
+    });
 });
 
-app.post("/add", (req, res) => {
-  res.send("전송완료");
-  console.log(req.body.title);
-  console.log(req.body.date);
+app.delete("/delete", function (req, res) {
+  console.log(req.body);
+  req.body._id = parseInt(req.body._id);
+  db.collection("post").deleteOne(req.body, function (error, result) {
+    console.log("삭제완료");
+    res.status(200).send({ message: "성공했습니다" });
+  });
+});
+
+app.get("/detail/:id", (req, res) => {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    function (error, result) {
+      res.render("detail.ejs", { data: result });
+    }
+  );
 });
